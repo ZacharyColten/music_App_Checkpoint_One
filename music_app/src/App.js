c23515c964a52bc1df181ce1d6ac1b6ab76181cb
zacharycolten <zacharycolten@gmail.com>
2020-02-14 16:32:29 -0600

    not turn in ready

--- music_app/src/App.js
+++ music_app/src/App.js
@@ -1,31 +1,74 @@
 import React, { Component } from 'react';
+import { AppBar, Toolbar, Typography, Snackbar } from '@material-ui/core';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
-import UserForm from './components/UserForm'
-
-import './App.css';
 
 class App extends Component {
   state = {
     loggedIn: false,
-    online: false,
-    notifications: [
-      {message: "Your application is offline. You won't be able to share or stream music to other devices."},
-       {message: "Listening to music at a high volume could cause long-term hearing loss."},
-        {message: "Music quality is degraded. Increase quality if your connection allows it."}
-      ]
-    
-
+    online: true,
+    notification: '',
+    volume: 0,
+    quality: 3
   }
 
-
-  render() {
-   return (
-    <div className="App">
-      <UserForm/>
-      
-    </div>
-  );
+  login = (e) => {
+    e.preventDefault()
+    this.setState({ loggedIn: true })
   }
+
+  toggleOnline = (e) => {
+    let online = !this.state.online;
+    this.setState({
+      online,
+      notification: online ? '' : 'Your application is offline. You wont be able to share or stream music to other devices.'
+    })
+  }
+
+  changeVolume = (e, volume) => {
+    this.setState({
+      volume: volume,
+      notification: volume < 80 ? '' : 'Listening to music at a high volume could cause long-term hearing loss.'
+    })
+  }
+
+  handleClose = () => {
+    this.setState({notification: '' })
+  }
+
+  changeQuality = (quality) => { 
+    this.setState({
+      quality: quality,
+      notification: quality > 1 ? '' : 'Music quality is degraded. Increase quality if your connection allows it.'
+    })
+  }
+
+  render = () => {
+
+    return (
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">
+              Music App
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        {this.state.loggedIn ?
+          <Dashboard
+            checked={this.state.online}
+            toggleOnline={this.toggleOnline}
+            volume={this.state.volume}
+            changeVolume={this.changeVolume}
+            changeQuality={this.changeQuality}
+            quality={this.state.quality}
+          /> : <Login onSubmit={this.login} />}
+        <Snackbar open={!!this.state.notification} onClose={this.handleClose} message={this.state.notification} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ music_app/src/components/Dashboard.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react'
+import { Switch, Slider } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+
+
+
+
+export class Dashboard extends Component {
+
+  render() {
+    var style = {
+      "display": "flex", "flex-direction": "row", "position": "absolute",
+      "left": "50%", top: "50%", "transform": "translate(-50%, -50%)"
+    };
+
+    return (
+
+      <div style={style}>
+
+
+        <Card style={{ "margin": "0" }}>
+          <CardContent style={{
+            "display": "flex",
+            "align-items": "center",
+            "justify-content": "center", "flex-direction": "column"
+          }}>
+            <Typography>
+              Toggle Online Status
+              <br></br><br></br>
+            </Typography>
+            <Switch
+              checked={this.props.checked}
+              onChange={this.props.toggleOnline}
+              value="online"
+              inputProps={{ 'aria-label': 'secondary checkbox' }}
+            />
+          </CardContent>
+        </Card>
+
+
+
+        <Card style={{ "margin": "0" }}>
+          <CardContent>
+            <Typography style={{
+              "display": "flex",
+              "align-items": "center",
+              "justify-content": "center"
+            }} id="discrete-slider-always" gutterBottom>
+              Volume
+            </Typography>
+            <Slider
+              defaultValue={this.props.volume}
+              step={10}
+              min={0}
+              max={100}
+              onChange={this.props.changeVolume}
+            />
+            <siv>{this.props.volume}</siv>
+          </CardContent>
+        </Card>
+
+
+
+        <Card style={{ "margin": "0" }}>
+          <CardContent>
+            <Typography style={{
+              "display": "flex",
+              "align-items": "center",
+              "justify-content": "center"
+            }}>
+              Quality
+            </Typography>
+            <FormControl >
+              <Select
+                labelId="demo-simple-select-placeholder-label-label"
+                id="demo-simple-select-placeholder-label"
+                defaultValue={this.props.quality}
+                onChange={this.props.changeQuality}
+              >
+                <MenuItem value={1}>Low</MenuItem>
+                <MenuItem value={2}>Normal</MenuItem>
+                <MenuItem value={3}>High</MenuItem>
+              </Select>
+            </FormControl>
+          </CardContent>
+        </Card>
+      </div>
+
+
+
+
+
+    )
+
+  }
+}
+export default Dashboard
